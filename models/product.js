6a3abe34e986c658637a5391a5fc8266a820c3f1
kigaliphonecenter <kigaliphonecenter@gmail.com>
2020-10-04 15:41:43 +0200

    first commit

---
+++ models/product.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const {
+    ObjectId
+} = mongoose.Schema;
+
+const productSchema = mongoose.Schema({
+    writer: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    brand: {
+        type: String,
+        maxlength: 50
+    },
+
+    short_description: {
+        type: String
+
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    quantity: {
+        type: Number
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+
+
+}, {
+    timestamps: true
+})
+
+
+productSchema.index({
+    title: 'text',
+    description: 'text',
+}, {
+    weights: {
+        title: 5,
+        description: 1,
+    }
+})
+
+module.exports = mongoose.model("Product", productSchema);
